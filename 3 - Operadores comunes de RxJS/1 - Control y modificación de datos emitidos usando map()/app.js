50a4d6e568ec9dbac07912911ef17b8cbde84718
Yeampier2022 <yeampierjesusa@gmail.com>
2024-01-02 15:23:43 -0400

    feat: operator map

--- 3 - Operadores comunes de RxJS/1 - Control y modificación de datos emitidos usando map()/app.js
+++ 3 - Operadores comunes de RxJS/1 - Control y modificación de datos emitidos usando map()/app.js
@@ -3,8 +3,10 @@ const numeros = Rx.Observable.from([10, 100, 1000]);
 numeros
     .map(numero => {
         console.log('Antes: ', numero);
-        return numero + 5;
+        return numero * 5;
     })
     .subscribe(dato => {
         console.log('Después: ', dato)
     })
+
+    // Este operador puede modificar el dato que reciba y al mostrarlo llega con el nuevo valor
